13fa837614201a2e35484f59c92853de6a59f15c
Keith <keithlimek96@gmail.com>
2025-12-24 02:50:26 +0800

    added the first post path for users

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,7 +10,14 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
+	"github.com/keithfy96/go-project/internal/database"
+
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 	godotenv.Load(".env")
@@ -18,6 +26,22 @@ func main() {
 	if portString == "" {
 		log.Fatal("PORT is not set")
 	}
+
+	dbURL := os.Getenv("DB_URL")
+	if dbURL == "" {
+		log.Fatal("DB_URL is incorrect")
+	}
+
+	conn, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("Can't connect to a database: ", err)
+	}
+	log.Println("database opened")
+
+	apiCfg := apiConfig{
+		DB: database.New(conn),
+	}
+	log.Println("api config created & database connected")
 
 	router := chi.NewRouter()
 
@@ -33,6 +57,7 @@ func main() {
 	v1Router := chi.NewRouter()
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/err", handlerErr)
+	v1Router.Post("/users", apiCfg.handlerCreateUser)
 
 	router.Mount("/v1", v1Router)
 
@@ -43,7 +68,7 @@ func main() {
 
 	log.Printf("Server is runnign on port %s", portString)
 
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
